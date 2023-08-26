87fd2133ee8419514dbf4a35f5e90b2450ff1883
shaunprice <shaundnz@gmail.com>
2023-08-26 19:09:42 +1200

    Removed unused imports

--- tests/chat.ts
+++ tests/chat.ts
@@ -1,5 +1,5 @@
 import { expect } from '@playwright/test';
-import type { Locator, Page } from '@playwright/test';
+import type { Page } from '@playwright/test';
 
 export class ChatPage {
 	readonly page: Page;
