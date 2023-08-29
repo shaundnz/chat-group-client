225db23868ca928cbba4543351bb7f6559fab14f
shaunprice <shaundnz@gmail.com>
2023-08-29 22:25:44 +1200

    Updated api fetchers to user custom fetch with error handling

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,22 @@
 import type { Channel } from '$lib/types';
 import type { PageLoad } from './$types';
 import { ChannelsApi } from '$lib/api';
+import { redirect } from '@sveltejs/kit';
+import { HttpError } from '$lib/api/HttpError';
 
 export const load: PageLoad<{ channels: Channel[]; defaultChannel: Channel }> = async () => {
-	const defaultChannel = await ChannelsApi.getDefaultChannel();
-	const allChannels = await ChannelsApi.getAllChannels();
-	return {
-		channels: allChannels,
-		defaultChannel: defaultChannel
-	};
+	try {
+		const defaultChannel = await ChannelsApi.getDefaultChannel();
+		const allChannels = await ChannelsApi.getAllChannels();
+
+		return {
+			channels: allChannels,
+			defaultChannel: defaultChannel
+		};
+	} catch (err) {
+		if (err instanceof HttpError) {
+			if (err.statusCode === 401) throw redirect(307, '/auth/login');
+		}
+		throw err;
+	}
 };
