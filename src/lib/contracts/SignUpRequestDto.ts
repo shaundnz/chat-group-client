e375e357d7ff6ea85d1e54a676f3ea6f1619b56e
shaunprice <shaundnz@gmail.com>
2023-08-31 22:47:37 +1200

    Created auth dtos

---
+++ src/lib/contracts/SignUpRequestDto.ts
@@ -0,0 +1,5 @@
+export interface SignUpRequestDto {
+	username: string;
+	password: string;
+	confirmPassword: string;
+}
--- src/lib/contracts/index.ts
+++ src/lib/contracts/index.ts
@@ -6,3 +6,7 @@ export * from './SendMessageEventResponseDto';
 export * from './ReceivedMessageEventResponseDto';
 export * from './ChannelCreatedEventResponseDto';
 export * from './ErrorDto';
+export * from './LoginRequestDto';
+export * from './LoginResponseDto';
+export * from './SignUpRequestDto';
+export * from './UserDto';
