21a7f91ff1c224f3ff8d9b70dff7ebab94f4e3d2
shaunprice <shaundnz@gmail.com>
2023-09-02 13:33:52 +1200

    Added user to layout load function to set auth context

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import { createAuthContext } from '$lib/context/authContext';
+	import { HttpError } from '$lib/api';
+	import { errorToast } from '$lib/utils';
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	createAuthContext(data.user);
+
+	const handleError = (err: Error) => {
+		if (err instanceof HttpError) {
+			if (err.statusCode === 401) {
+				errorToast('Authentication error');
+			}
+		}
+	};
 </script>
 
+<svelte:window on:unhandledrejection|capture={(e) => handleError(e.reason)} />
 <slot />
+<SvelteToast />
---
+++ src/routes/+layout.ts
@@ -0,0 +1,19 @@
+import { AuthApi, HttpError } from '$lib/api';
+import type { User } from '$lib/types';
+import type { PageLoad } from './$types';
+
+export const ssr = false;
+
+export const load: PageLoad<{ user: User | null }> = async () => {
+	try {
+		const user = await AuthApi.getLoggedInUser();
+		return {
+			user: user
+		};
+	} catch (err) {
+		if (err instanceof HttpError) {
+			if (err.statusCode === 401) return { user: null };
+		}
+		throw err;
+	}
+};
